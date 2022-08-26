6a45ffb37bedcb29e120481ceeb944745a9c6837
HS980924 <choihs980924@naver.com>
2022-08-26 16:15:05 +0900

    complete Login function

--- FE/my-app/pages/login.js
+++ FE/my-app/pages/login.js
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import styles from "../styles/Login.module.css";
 
+const URL = 'http://localhost:5000/auth/login';
+axios.defaults.withCredentials = true;
+
 function Login() {
+
+    const [ID, setID] = useState("")
+    const [PW, setPW] = useState("")
+
+    function loginUser(){
+        const body = { ID, PW }
+        const request = axios.post(URL, body)
+            .then(res => {
+                console.log(res);
+                if(res.status === 200){
+                    console.log("======================", "로그인 성공");
+                    window.location.assign('http://localhost:3000/main');
+                }else{
+                    alert(res.data);
+                }
+            });
+    }
+
+    const onIdHandler = (e) => {
+        console.log(ID);
+        setID(e.currentTarget.value)
+    }
+
+    const onPwHandler = (e) => {
+        console.log(PW);
+        setPW(e.currentTarget.value)
+    }
+
+    const onSubmitHandler = (e) => {
+        console.log('start');
+        e.preventDefault();
+        loginUser();
+    }
+
     return (
         <>
             <div className={styles.LoginForm}>
                 <p className={styles.Banner}>Adminstrator</p>
-                <div className={styles.Info}>
+                <form className={styles.Info} onSubmit={onSubmitHandler}>
                     <div className={styles.InfoInput}>
-                        <input type="text" placeholder="Admin ID" />
-                        <input type="text" placeholder="Admin PW" />
+                        <input type="text" value = {ID} placeholder="Admin ID" onChange={onIdHandler} required/>
+                        <input type="password" value = {PW} placeholder="Admin PW" onChange={onPwHandler} required/>
                     </div>
                     <div className={styles.InfoButton}>
-                        <button>Login</button>
+                        <button type="submit">Login</button>
                     </div>
-                </div>
+                </form>
             </div>
         </>
     );
