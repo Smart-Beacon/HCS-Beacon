98d70fbca6730cc2df797c916311cf2f90b53783
HS980924 <choihs980924@naver.com>
2022-08-26 20:19:02 +0900

    complete GET door API

---
+++ BE/src/routes/door.js
@@ -0,0 +1,57 @@
+const express = require('express');
+
+const getMainDatas = require('../service/door.js');
+const checkAdmin = require('../service/check.js');
+
+const router = express.Router();
+
+// 실시간 감시 현황 페이지 데이터 요청
+// 건물명, 출입문 명, 비콘ID, 현재상태, 개방시간, 폐쇄시간, 경보상태를 Json 데이터로 전송
+router.get('/', async(req,res,next) =>{
+    const { id, isSuper } = req.signedCookies.accessToken;
+    console.log(id, isSuper);
+    try{
+        const check = await checkAdmin.checkAdmin(id,isSuper);
+        if (check !== 2){
+            const data = await getMainDatas.getAllDoorData();
+            console.log(data);
+            res.json(data);
+        }else{
+            res.status(400).send('Not Found Admin');
+        }
+    }catch(err){
+        res.status(400).send(err.message);
+    }
+});
+
+// 출입문 관리 설정 페이지 데이터 요청
+// 건물명, 출입문 명, 비콘ID, 현재상태, 출입관리, 개방시간, 폐쇄시간를 Json 데이터로 전송
+router.get('/admin', async(req,res,next) => {
+    const { id, isSuper } = req.signedCookies.accessToken
+    console.log(id, isSuper);
+    try{
+        const check = await checkAdmin.checkAdmin(id,isSuper);
+        if(check === 0){
+            const data = await getMainDatas.getSuperDoorDatas();
+            res.json(data);
+        }else if(check === 1){
+            const data = await getMainDatas.getAdminDoorDatas(id);
+            res.json(data);
+        }else{
+            res.status(400).send('Not Found Admin');
+        }
+    }catch(err){
+        res.status(400).send(err.message);
+    }
+});
+
+// 질문
+// 출입문 관리 설정 (등록 모달)
+// 담당관리자만 해당 
+// 
+router.post('/admindoor',async(req,res,next)=>{
+
+
+});
+
+module.exports = router;
---
+++ BE/src/service/door.js
@@ -0,0 +1,126 @@
+const Door =  require('../db/models/door');
+const Statement = require('../db/models/statement');
+const AdminStatement = require('../db/models/adminStatement');
+const AdminDoor = require('../db/models/adminDoor');
+
+// GET : 실시간 관리 현황 데이터
+// 모든 건물에 있는 도어들의 데이터들을 확인하는 함수
+// 최고 관리자 및 중간 관리자 모두 사용하는 함수
+// 건물명, 출입문 명 ID, 현재상태, 개방시간, 폐쇄시간, 경보상태
+const getAllDoorData = async() =>{
+    const stateIds = await Statement.findAll();
+
+    const doorDatas = await Promise.all(
+        stateIds.map(async stateData => {
+            const doorIds = await Door.findAll({
+                where: {staId:stateData.staId},
+                attributes: ['doorId','doorName','isOpen','warning','openTime','closeTime']
+            });
+
+            const setdDoorData = await Promise.all(
+                doorIds.map(async doorData =>{
+                    const setData = {
+                        staName: stateData.staName,
+                        doorName: doorData.doorName,
+                        doorId: doorData.doorId,
+                        isOpen: doorData.isOpen,
+                        openTime: doorData.openTime,
+                        closeTime: doorData.closeTime,
+                        warning: doorData.warning,
+                    }
+                    return setData
+                }));
+
+            return setdDoorData
+        })
+    )
+
+    const result = await doorDatas.flatMap(data => data);
+
+    return result;
+}
+
+// GET : 출입문 관리 설정 데이터
+// 모든 건물에 있는 도어들의 데이터들을 확인하는 함수
+// 최고 관리자만 사용하는 함수
+// 건물명, 출입문 명 ID, 현재상태, 출입관리, 날짜, 개방시간, 폐쇄시간
+const getSuperDoorDatas = async() =>{
+    const stateIds = await Statement.findAll();
+
+    const doorDatas = await Promise.all(
+        stateIds.map(async stateData => {
+            const doorIds = await Door.findAll({
+                where: {staId:stateData.staId},
+                attributes: ['doorId','doorName','isOpen','isMonitoring','latestDate','openTime','closeTime']
+            });
+
+            const setdDoorData = await Promise.all(
+                doorIds.map(async doorData =>{
+                    const setData = {
+                        staName: stateData.staName,
+                        doorName: doorData.doorName,
+                        doorId: doorData.doorId,
+                        isMonitoring: doorData.isMonitoring,
+                        latestDate: doorData.latestDate,
+                        openTime: doorData.openTime,
+                        closeTime: doorData.closeTime,
+                    }
+                    return setData
+                }));
+
+            return setdDoorData
+        })
+    )
+
+    const result = await doorDatas.flatMap(data => data);
+
+    return result;
+}
+
+// GET : 출입문 관리 설정 데이터
+// 모든 건물에 있는 도어들의 데이터들을 확인하는 함수
+// 중간 관리자만 사용하는 함수
+// 건물명, 출입문 명 ID, 현재상태, 출입관리, 날짜, 개방시간, 폐쇄시간
+const getAdminDoorDatas = async(adminId) =>{
+    const doorIds = await  AdminDoor.findAll({
+        where:{ adminId },
+        attributes:['doorId'],
+    });
+
+    const doorDatas = await Promise.all(
+        doorIds.map(async doorId =>{
+
+            const doorData = await Door.findOne({
+                where:{doorId:doorId.doorId}
+            });
+
+            const statmentName = await Statement.findOne({
+                where:{staId:doorData.staId},
+                attributes: ['staName'],
+            });
+
+            const result = {
+                staName: statmentName.staName,
+                doorName: doorData.doorName,
+                doorId: doorData.doorId,
+                isOpen: doorData.isOpen,
+                isMonitoring: doorData.isMonitoring,
+                latestDate: doorData.latestDate,
+                openTime: doorData.openTime,
+                closeTime: doorData.closeTime,
+                
+            };
+
+            return result;
+        })
+    );
+
+    return doorDatas
+}
+
+
+module.exports = {
+    getAllDoorData,
+    getAdminDoorDatas,
+    getSuperDoorDatas,
+}
