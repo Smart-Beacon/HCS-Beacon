614ad707936b8c01e2a011fe5dc2ce7d2cc2fe45
MinsungKimDev <minsungkim.dev@gmail.com>
2022-09-04 16:30:47 +0900

    Add Time exception

--- BE/src/service/time.js
+++ BE/src/service/time.js
@@ -1,23 +1,36 @@
-const getDate = (date) => {
-    var alertYear = date.getFullYear();
-    var alertMonth = ('0' + (date.getUTCMonth() + 1)).slice(-2);
-    var alertDay = ('0' + date.getUTCDate()).slice(-2);
+const e = require("express");
 
-    return alertYear + '.' + alertMonth + '.' + alertDay;
+const getDate = (date) => {
+    if(date === null){
+        return null;
+    } else{
+        var alertYear = date.getFullYear();
+        var alertMonth = ('0' + (date.getUTCMonth() + 1)).slice(-2);
+        var alertDay = ('0' + date.getUTCDate()).slice(-2);
+
+        return alertYear + '.' + alertMonth + '.' + alertDay;
+    }
 }
 
 const getTime = (date) => {
-
-    var hours = ('0' + date.getUTCHours()).slice(-2); 
-    var minutes = ('0' + date.getUTCMinutes()).slice(-2);
-    return hours + ':' + minutes;
+    if(date === null){
+        return null;
+    } else{
+        var hours = ('0' + date.getUTCHours()).slice(-2); 
+        var minutes = ('0' + date.getUTCMinutes()).slice(-2);
+        return hours + ':' + minutes;
+    }
 }
 
 const getTimeSecond = (date) =>{
-    var hours = ('0' + date.getUTCHours()).slice(-2); 
-    var minutes = ('0' + date.getUTCMinutes()).slice(-2);
-    var seconds = ('0' + date.getUTCSeconds()).slice(-2);
-    return hours + ':' + minutes + ':' + seconds;
+    if(date === null){
+        return null;
+    } else{
+        var hours = ('0' + date.getUTCHours()).slice(-2); 
+        var minutes = ('0' + date.getUTCMinutes()).slice(-2);
+        var seconds = ('0' + date.getUTCSeconds()).slice(-2);
+        return hours + ':' + minutes + ':' + seconds;
+    }
 }
 
 module.exports = {
