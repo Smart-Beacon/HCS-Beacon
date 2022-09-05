38d4b28080b4d2e4e2bcfc55774f433bb12f7565
HS980924 <choihs980924@gmail.com>
2022-09-06 02:49:28 +0900

    Delete Reduplication of Code

--- BE/src/routes/super.js
+++ BE/src/routes/super.js
@@ -2,23 +2,18 @@ const express = require('express');
 
 const checkAdmin = require('../service/check.js');
 const getAdminDatas = require('../service/super.js');
-const getMainDatas = require('../service/door.js');
 
 const router = express.Router();
 
 router.get('/admins',async(req,res,next) => {
-    const { id, isSuper } = req.signedCookies.accessToken;
-    console.log(id, isSuper);
     try{
+        const id = req.signedCookies.accessToken;
+        const isSuper = req.cookies.isSuper;
+        console.log(id, isSuper);
         const check = await checkAdmin.checkAdmin(id,isSuper);
         if (check === 0){
-            const doorData = await getMainDatas.getAllDoorData();
             const adminData = await getAdminDatas.getAdminData();
-            const result = {
-                doorData,
-                adminData,
-            };
-            res.json(result);
+            res.json(adminData);
         }else{
             res.status(400).send('Not Found SuperAdmin');
         }
@@ -28,9 +23,10 @@ router.get('/admins',async(req,res,next) => {
 });
 
 router.post('/admin/register',async(req,res,next) =>{
-    const { id, isSuper } = req.signedCookies.accessToken;
-    console.log(id, isSuper);
     try{
+        const id = req.signedCookies.accessToken;
+        const isSuper = req.cookies.isSuper;
+        console.log(id, isSuper);
         const check = await checkAdmin.checkAdmin(id,isSuper);
         if (check === 0){
             const adminData = await getAdminDatas.createAdminData(req.body);
