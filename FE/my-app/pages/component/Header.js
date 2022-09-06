bd8fba0b213aad776ee51a0e5cbe686f5c3f6cb9
Choi HyungSoon <choihs980924@gmail.com>
2022-09-06 20:24:19 +0900

    Update Header.js

--- FE/my-app/pages/component/Header.js
+++ FE/my-app/pages/component/Header.js
@@ -52,8 +52,6 @@ const style = css`
 
 function Header() {
   const [Data, setData] = useState("")
-
-  let timer = null;
   const [time, setTime] = useState(moment());
   useEffect(() => {
     const key = process.env.NEXT_PUBLIC_CRYPTO_KEY;
@@ -62,6 +60,7 @@ function Header() {
     const originalText = JSON.parse(bytes.toString(crypto.enc.Utf8));
     console.log(originalText);
     setData(originalText);
+    let timer = null;
     timer = setInterval(() => {
       setTime(moment());
     }, 1000);
