388ddd76afa5497c3c8b24099434f3b8cac38888
HS980924 <choihs980924@naver.com>
2022-09-27 22:37:22 +0900

    create app statement  data

--- BE/src/routes/statement.js
+++ BE/src/routes/statement.js
@@ -31,5 +31,16 @@ router.post('/',async(req,res,next) =>{
     }
 });
 
+router.post('/regist',async(req,res,next) =>{
+    try{
+        const result = {
+            staData: await getStatementOfSuper(),
+            doorData: await getDoorOfSuper(),
+        }
+        res.status(200).json(result);
+    }catch(err){
+        res.status(400).send(err.message);
+    }
+});
 module.exports = router;
 
