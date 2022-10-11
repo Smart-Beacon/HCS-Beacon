17847f277c23b9888636af0c932cc255e8e0bcaf
HS980924 <choihs980924@gmail.com>
2022-10-12 02:40:50 +0900

    update door open socket emit

--- BE/src/socket.js
+++ BE/src/socket.js
@@ -3,18 +3,18 @@ const Door = require('./db/models/door');
 
 module.exports = (server, app) =>{
     const io = SocketIO(server,{path:'/socket.io'});
+    //path 수정 필요
     app.set('io',io);
 
     io.on('connection',(socket) =>{
         const req = socket.request;
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속',ip, socket.id, req.ip);
+        console.log(`새로운 클라이언트 접속 IP Address: ${ip}, Socket Id: ${socket.id}`);
         
         socket.emit('test',"test");
 
-
-        socket.on('disconnec',()=>{
-            console.log('클라이언트 접속 해제',ip,socket.id);
+        socket.on('disconnect',()=>{
+            console.log(`클라이언트 접속 해제  IP Address: ${ip}, Socket Id: ${socket.id}`);
             clearInterval(socket.interval);
         });
 
@@ -27,27 +27,39 @@ module.exports = (server, app) =>{
             const exDoor = await Door.findOne({where:{doorId:data.doorId}});
             if(exDoor){
                 exDoor.socketId = socket.id;
-                exDoor.isOpen = data.isOpen;
                 await exDoor.save();
             }else{
                 console.log('error');
             }
 
+            console.log(data.doorId, socket.id);
+        });
+
+        socket.on('open',async (data)=>{
+            const exDoor = await Door.findOne({where:{doorId:data.doorId}});
+            if(exDoor){
+                exDoor.isOpen = data.isOpen;
+                //exDoor.isOpen = true;
+                exDoor.socketId = socket.id;
+                await exDoor.save();
+            }else{
+                console.log('error');
+            }
             console.log(data.doorId, data.isOpen);
         });
 
-        // socket.on('open',async (data)=>{
-        //     const exDoor = await Door.findOne({where:{doorId:data.doorId}});
-        //     if(exDoor){
-        //         exDoor.isOpen = data.isOpen;
-        //         exDoor.socketId = socket.id;
-        //         await exDoor.save();
-        //     }else{
-        //         console.log('error');
-        //     }
-
-        //     console.log(data.doorId, data.isOpen);
-        // });
+        socket.on('close',async (data)=>{
+            const exDoor = await Door.findOne({where:{doorId:data.doorId}});
+            if(exDoor){
+                exDoor.isOpen = data.isOpen;
+                //exDoor.isOpen = false;
+                exDoor.socketId = socket.id;
+                await exDoor.save();
+            }else{
+                console.log('error');
+            }
+            console.log(data.doorId, data.isOpen);
+        });
 
     });
 };
